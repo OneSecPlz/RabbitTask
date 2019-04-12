60a9208523bb9a866e644964e5b37c0724842216
Быков Александр Сергеевич <abykov@stream.ru>
2019-04-12 12:16:19 +0300

    Refactored main

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 public class Main {
-    private static int N = 10, K = 3, count = 0;
+    private static int N = 4, K = 3, count = 0;
 
     public static void main(String[] args) {
-        for (int i = 1; i <= K; i++) {
-            if ((N - i) >= 0) {
-                System.out.println("первый прыжок " + i);
-                prygok(N - i, 2);
-            }
-        }
+        prygok(N, 1,"");
         System.out.println("Всего путей :" + count);
     }
 
-    private static void prygok(int remain, int prygokNumb) {
+    private static void prygok(int remain, int prygokNumb, String path) {
         for (int i = 1; i <= K; i++) {
             if (remain - i >= 0) {
-                System.out.println("Прыжок №" + prygokNumb + "на " + i + "ступеней");
-                prygok(remain - i, prygokNumb + 1);
+                System.out.println("Прыжок №" + prygokNumb + " на " + i + " ступеней");
+                prygok(remain - i, prygokNumb + 1,path+i+"+");
                 if ((remain - i) == 0) {
                     count++;
                     System.out.println("Найден путь №" + count);
+                    System.out.println("Путь:" + path+i);
                 }
             }
         }
